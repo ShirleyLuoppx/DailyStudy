d115f7f25f0d02be56954fffe6d9e5017e9f0362
luoxia <lx13527507647@sina.com>
2020-08-27 09:13:15 +0800

    feat: 添加了一个伪聊天界面的实现，主要是学到了一个方法：rv的定位到最后一行  rv.scrollToPosition(position:Int)
    添加了一个伪聊天界面的实现，主要是学到了一个方法：rv的定位到最后一行  rv.scrollToPosition(position:Int)

---
+++ app/src/main/java/com/ppx/dailystudy/chatview/ChatViewBean.kt
@@ -0,0 +1,16 @@
+package com.ppx.dailystudy.chatview
+
+/**
+ * Author: luoxia
+ * Date: 2020/8/26 23:22
+ * Description: 第二行代码-3.7-NinePatch的实例（本实例中并没有用到.9图相关的内容，只是单纯想实现一个这个聊天的样子而已）
+ */
+class ChatViewBean {
+    var msg: String = ""
+    var type: Int = -1
+}
+
+enum class ChatType(val type: Int) {
+    TYPE_RECIVE(0),
+    TYPE_SEND(1)
+}
