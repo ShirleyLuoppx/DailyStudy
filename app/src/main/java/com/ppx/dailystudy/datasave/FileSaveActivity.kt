ba3f1627a152a27f67af06683a3caee8a7c68438
luoxia <1184793244@qq.com>
2020-09-07 11:42:44 +0800

    feat: 添加了，文件存储方式的存取数据
    添加了，文件存储方式的存取数据(之前是因为那个While（readline！=null）的问题，但是具体为什么这样whiele就不行还没有深究)

--- app/src/main/java/com/ppx/dailystudy/datasave/FileSaveActivity.kt
+++ app/src/main/java/com/ppx/dailystudy/datasave/FileSaveActivity.kt
@@ -34,51 +34,57 @@ class FileSaveActivity : AppCompatActivity() {
      * 获取文件数据
      */
     private fun getFileData(): String {
-        var stringBuffer = StringBuffer()
+        val stringBuilder = StringBuilder()
         var bufferedReader: BufferedReader? = null
+
         try {
             val fileInputString = openFileInput("fileSave")
             bufferedReader = BufferedReader(InputStreamReader(fileInputString))
 
-            var str = ""
-            str = bufferedReader.readLine()
-            while (str != null) {
-                stringBuffer.append(str)
+            val str = bufferedReader.readLine()
+            if (str != null) {
+                stringBuilder.append(str)
             }
         } catch (e: Exception) {
             e.printStackTrace()
         } finally {
-            if (bufferedReader != null) {
-                try {
+            try {
+                if (bufferedReader != null) {
                     bufferedReader.close()
-                } catch (e: Exception) {
-                    e.printStackTrace()
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
-        Log.d("ippx", "onCreate: 获取到的数据为:${stringBuffer.toString()}")
-        return stringBuffer.toString()
+        Log.d("ippx", "onCreate: 获取到的数据为:${stringBuilder.toString()}")
+        return stringBuilder.toString()
     }
 
     /**
      * 文件存储数据
+     * 这里存储的文件，可以在--View-Tool Windows-Device File Explorer-data-data-项目包名-files中找到-右键open就可以看到你刚刚写入的数据啦
      */
     private fun save(data: String) {
         var bufferedWriter: BufferedWriter? = null
 //        val stringData = "this is a data for file saving"
         try {
+            /**
+             * 参数一：存储数据的文件的名字；
+             * 参数二：文件的操作方式，
+             * 有两种：MODE_PRIVATE，也是默认值，如果以前有重名的文件，里面的内容就会新的覆盖掉，如果没有就创建新的
+             * MODE_APPEND：从名字就可以看出来，是追加的，就是如果没有这个文件就创建新的，如果有这个文件新内容就会追加的以前的内容上
+             * 还有两个，MODE_WORLD_READABLE和MODE_WORLD_WRITEABLE，允许其他的app对我们的app进行读写文件操作，由于不安全，所以在Android4.2的时候被废弃了
+             */
             val outputStream = openFileOutput("fileSave", Context.MODE_PRIVATE)
             bufferedWriter = BufferedWriter(OutputStreamWriter(outputStream))
             bufferedWriter.write(data)
         } catch (e: Exception) {
             e.printStackTrace()
         } finally {
-            if (bufferedWriter != null) {
-                try {
-                    bufferedWriter.close()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
+            try {
+                bufferedWriter?.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
     }
