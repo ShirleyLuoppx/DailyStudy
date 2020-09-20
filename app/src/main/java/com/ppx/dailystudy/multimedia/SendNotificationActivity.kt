5568f04055d5dc01ee092312ccc5e92ea895dc32
luoxia <1184793244@qq.com>
2020-09-20 10:50:22 +0800

    feat: 添加了，手机设置消息通知的振动
    添加了，手机设置消息通知的振动

--- app/src/main/java/com/ppx/dailystudy/multimedia/SendNotificationActivity.kt
+++ app/src/main/java/com/ppx/dailystudy/multimedia/SendNotificationActivity.kt
@@ -3,16 +3,20 @@ package com.ppx.dailystudy.multimedia
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Vibrator
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import com.ppx.dailystudy.R
 import kotlinx.android.synthetic.main.activity_send_notification.*
+import java.io.File
 
 /**
  * Author: luoxia
@@ -21,10 +25,17 @@ import kotlinx.android.synthetic.main.activity_send_notification.*
  */
 class SendNotificationActivity : AppCompatActivity() {
 
+    /**
+     * NotificationManager是状态栏通知的管理类，负责发通知、清楚通知等
+     * 是一个系统Service，必须通过 getSystemService()方法来获取
+     */
+    var notificationManager: NotificationManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_notification)
 
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val id = "chat"
@@ -39,37 +50,55 @@ class SendNotificationActivity : AppCompatActivity() {
              * IMPORTANCE_HIGH：高级的重要程度，随便哪里都会显示，会有声音和震动，也可能会全屏显示
              * IMPORTANCE_MAX：没用过？？
              */
-            createNotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH)
+            createNotificationChannel(true, id, name, NotificationManager.IMPORTANCE_HIGH)
 
             val id2 = "subscribe"
             val name2 = "订阅消息"
-            createNotificationChannel(id2, name2, NotificationManager.IMPORTANCE_DEFAULT)
+            createNotificationChannel(true, id2, name2, NotificationManager.IMPORTANCE_DEFAULT)
         }
 
         bt_send_notification.setOnClickListener {
 
-            val pendingIntent = PendingIntent.getActivity(this,0, Intent(this,NotificationLayoutActivity::class.java),0)
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                Intent(this, NotificationLayoutActivity::class.java),
+                0
+            )
 
-            /**
-             * NotificationManager是状态栏通知的管理类，负责发通知、清楚通知等
-             * 是一个系统Service，必须通过 getSystemService()方法来获取
-             */
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             /**
              * notification是具体的状态栏通知对象，可以设置icon、文字、提示声音、振动等等参数
              */
-            val notification = NotificationCompat.Builder(this, "chat")//只传context的那种方法以及被废弃啦~
+            var builder: NotificationCompat.Builder? = null
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                builder = NotificationCompat.Builder(this, "chat")
+            } else {
+                builder = NotificationCompat.Builder(this)
+            }
+            val notification = builder//只传context的那种方法以及被废弃啦~
                 .setContentTitle("this is a chat for ppx")
                 .setContentText("您有一条新消息")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher_round))
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)//设置点击状态栏中的通知后自动在通知栏中取消这个通知
+                .setSound(Uri.fromFile(File("")))//可以设置通知的时间
+                //所以，这里表示，通知来临时，立即振动一秒，然后静止一秒，然后再振动一秒
+                .setVibrate(
+                    longArrayOf(
+                        0,
+                        1000,
+                        1000,
+                        1000
+                    )
+                )//设置振动的频率，第一个参数为通知来临时静止的时间，第二个表示振动的时间，第三个表示静止的时间，第四个表示振动的时间，以此类推
                 .build()
 //            notificationManager.cancel(1)   不知道这个为哈不行。
-            notificationManager.notify(1, notification)
+            notificationManager?.notify(1, notification)
+
+            platVibrate()
+
         }
 
         bt_send_subscribe_notification.setOnClickListener {
@@ -86,12 +115,29 @@ class SendNotificationActivity : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel(channelId: String, channelName: String, importance: Int) {
-
+    private fun createNotificationChannel(
+        isVibrate: Boolean,
+        channelId: String,
+        channelName: String,
+        importance: Int
+    ) {
         val channel = NotificationChannel(channelId, channelName, importance)
+        if (isVibrate) {
+            channel.enableVibration(true)//不起作用
+            channel.vibrationPattern = longArrayOf(0, 1000, 1000, 1000)
+        } else {
+            channel.enableVibration(false)
+            channel.vibrationPattern = longArrayOf(0)
+        }
+        notificationManager?.createNotificationChannel(channel)
+    }
 
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+    /**
+     * 来一个振动的方法
+     * 太奇怪了，enableVibration也不起作用，权限也给了，版本也判断了
+     */
+    private fun platVibrate() {
+        val vibrate = getSystemService(Service.VIBRATOR_SERVICE) as Vibrator
+        vibrate.vibrate(longArrayOf(0, 1000, 1000, 1000), -1)
     }
 }
