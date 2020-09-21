876104710770348311cea1ed27af951b9642e0d6
luoxia <1184793244@qq.com>
2020-09-21 23:32:52 +0800

    feat: 添加了，pull方式解析xml数据
    添加了，pull方式解析xml数据

--- app/src/main/java/com/ppx/dailystudy/cha09internet/OkHttpActivity.kt
+++ app/src/main/java/com/ppx/dailystudy/cha09internet/OkHttpActivity.kt
@@ -1,10 +1,15 @@
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ppx.dailystudy.R
 import kotlinx.android.synthetic.main.activity_okhttp.*
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.StringReader
+import java.lang.Exception
 
 /**
  * Author: LuoXia
@@ -22,6 +27,9 @@ class OkHttpActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 使用okhttp3发送一个网络请求连接到百度首页
+     */
     private fun sendOkHttpRequest() {
         Thread(Runnable {
             val okHttpClient = OkHttpClient()
@@ -41,6 +49,63 @@ class OkHttpActivity : AppCompatActivity() {
             tv_content.post {
                 tv_content.text = postResponse.body?.string()
             }
+
+            postResponse.body?.string()?.let {
+                parseXmlWithPull(it)
+            }
+
         }).start()
+    }
+
+    /**
+     * pull方式解析xml数据(需要本地自己搭建一个简单的服务器，从服务器上拉取数据)
+     */
+    private fun parseXmlWithPull(xmlData: String) {
+        try {
+            //实例化一个xml的pull方式的解析工厂
+            val xmlPullParserFactory = XmlPullParserFactory.newInstance()
+            //通过工厂实例化一个xmlpull方式的解析器
+            val xmlPullParser = xmlPullParserFactory.newPullParser()
+            xmlPullParser.setInput(StringReader(xmlData))
+
+            //获取他的事件类型
+            var eventType = xmlPullParser.eventType
+
+            var id = ""
+            var name = ""
+            var version = ""
+
+            //不是最后一个节点就继续解析
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+
+                val nodeName = xmlPullParser.name
+
+                when (eventType) {
+                    //开始解析某个节点
+                    XmlPullParser.START_TAG -> {
+                        when (nodeName) {
+                            "id" -> {
+                                id = xmlPullParser.nextText()
+                            }
+                            "name" -> {
+                                name = xmlPullParser.nextText()
+                            }
+                            "version" -> {
+                                version = xmlPullParser.nextText()
+                            }
+                        }
+                    }
+                    //完成解析某个节点
+                    XmlPullParser.END_TAG -> {
+                        if (nodeName == "app") {
+                            Log.d("ippx", "parseXmlWithPull: id=$id,name=$name,version=$version")
+                        }
+                    }
+                }
+                eventType = xmlPullParser.next()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
