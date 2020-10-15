f78b4d1e6a35ca105b719192bd1883e3eabee2cb
luoxia <1184793244@qq.com>
2020-10-15 17:39:26 +0800

    feat: 添加一个可折叠式标题栏的布局（功能还未完成）
    添加一个可折叠式标题栏的布局（功能还未完成）

---
+++ app/src/main/java/com/ppx/dailystudy/chap12materialdesign/CollaspcingToobarActivity.kt
@@ -0,0 +1,18 @@
+package com.ppx.dailystudy.chap12materialdesign
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ppx.dailystudy.R
+
+/**
+ * Author: LuoXia
+ * Date: 2020/10/15 11:09
+ * Description: CollapsingTooBarLayout的简单使用
+ */
+class CollaspcingToobarActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collaspcing_toolbar_layout)
+    }
+}
