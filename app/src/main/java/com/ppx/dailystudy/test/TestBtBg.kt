b29eefffe3a92b71f97e5ec54181189d066b723f
luoxia <1184793244@qq.com>
2020-10-27 18:22:33 +0800

    feat: 添加了一些测试类
    添加了一些测试类

---
+++ app/src/main/java/com/ppx/dailystudy/test/TestBtBg.kt
@@ -0,0 +1,18 @@
+package com.ppx.dailystudy.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ppx.dailystudy.R
+
+/**
+ * Author: LuoXia
+ * Date: 2020/10/26 18:23
+ * Description: DESCRIPTION
+ */
+class TestBtBg : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_bg)
+    }
+}
---
+++ app/src/main/java/com/ppx/dailystudy/test/TimePickerDemoActivity.kt
@@ -0,0 +1,24 @@
+package com.ppx.dailystudy.test
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.ppx.dailystudy.R
+import kotlinx.android.synthetic.main.activity_timepicker_demo.*
+
+/**
+ * Author: LuoXia
+ * Date: 2020/10/27 10:06
+ * Description: DESCRIPTION
+ */
+class TimePickerDemoActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timepicker_demo)
+
+        timepicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+            Log.d("ippx", "onCreate: $hourOfDay---$minute")
+        }
+    }
+}
