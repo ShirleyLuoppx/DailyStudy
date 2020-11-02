049fe82f9e998ce1a2b7e71d41c6e672e98147fc
luoxia <1184793244@qq.com>
2020-11-02 23:38:26 +0800

    docs: 添加一个多窗口切换相关知识的注释
    添加一个多窗口切换相关知识的注释

--- app/src/main/java/com/ppx/dailystudy/chap13advancedtechniques/LongRunningService.kt
+++ app/src/main/java/com/ppx/dailystudy/chap13advancedtechniques/LongRunningService.kt
@@ -7,6 +7,18 @@ import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 
+/**
+ * 加一个闹钟服务
+ * 一些多窗口模式的知识：
+ * 1、应用是默认支持多窗口切换的，tarGetsdkVersion>24的时候可以在application使用   android:resizeableActivity="false"  来禁用多窗口模式
+ * 2、如果是targetsdkVersion<24的时候，也可以在活动上指明：android:screenOrientation="portrait"  只 支持竖屏，android:screenOrientation="landscape"只支持横屏
+ * 3、多窗口切换的时候的生命周期变化：例如，A应用多窗口切到B应用再到A，
+ *  启动A：此时的生命周期变化为-->A onCreate--> A onStart -->  A onResume-->
+ *  进入多窗口模式：  A onPause -->  A onStop --> A onDestroy -->   A onCreate--> A onStart -->  A onResume-->  A onPause    相当于经历了一次重建，因为进入多窗口模式活动大小发生较明显改变，默认重建活动
+ *  进入B应用：  B onCreate --> B onStart --> A onResume
+ *  再切到A ：   B  onPause         A  onResume
+ *  也可以在activity使用     android:configChanges="keyboardHidden|screenSize|screenLayout|orientation"  来阻止活动的重建，而转到  onConfigurationChanged() 里面去处理屏幕变化情况
+ */
 class LongRunningService : Service() {
 
     private val TAG = "LongRunningService"
