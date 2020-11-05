861e98fe7a58b530491bb12983095131351e0ace
luoxia <1184793244@qq.com>
2020-11-05 09:17:36 +0800

    删除了重新提交测试git

--- app/src/main/java/com/ppx/dailystudy/test/TestDemo.kt
+++
@@ -1,9 +0,0 @@
-package com.ppx.dailystudy.test
-
-/**
- * Author: LuoXia
- * Date: 2020/11/5 9:10
- * Description: DESCRIPTION
- */
-class TestDemo {
-}
--- app/src/main/java/com/ppx/dailystudy/test/TestGit.kt
+++
@@ -1,9 +0,0 @@
-package com.ppx.dailystudy.test
-
-/**
- * Author: LuoXia
- * Date: 2020/11/5 9:09
- * Description: 做一个git提交时候的测试===========
- */
-class TestGit {
-}
