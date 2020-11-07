699c4933cea5aafcbc90e3bf023fb3b66b904b5f
luoxia <1184793244@qq.com>
2020-11-07 20:31:11 +0800

    进入项目--酷欧天气实战：添加数据库和对应的配置

---
+++ app/src/main/java/com/ppx/dailystudy/chap14coolweather/CoolWeatherMainActivity.kt
@@ -0,0 +1,9 @@
+package com.ppx.dailystudy.chap14coolweather
+
+/**
+ * Author: LuoXia
+ * Date: 2020/11/7 20:07
+ * Description: 项目开发：酷欧天气首页
+ */
+class CoolWeatherMainActivity {
+}
--- app/src/main/java/com/ppx/dailystudy/test/TestDemo.kt
+++ app/src/main/java/com/ppx/dailystudy/test/TestDemo.kt
@@ -1,9 +1,22 @@
 package com.ppx.dailystudy.test
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ppx.dailystudy.R
+import kotlinx.android.synthetic.main.activiyu_timepicker_test.*
+
 /**
  * Author: LuoXia
  * Date: 2020/11/5 9:18
  * Description: 111
  */
-class TestDemo {
+class TestDemo : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiyu_timepicker_test)
+
+        timepicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+
+        }
+    }
 }
