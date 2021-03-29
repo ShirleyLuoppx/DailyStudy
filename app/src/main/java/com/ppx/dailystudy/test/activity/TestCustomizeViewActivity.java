136d8ce3cd67255b03193966353d790502ee2390
luoxia <lx13627507647@sina.com>
2021-03-29 18:08:25 +0800

    修改   地图 包加载错误问题（没得定位阿）

---
+++ app/src/main/java/com/ppx/dailystudy/test/activity/TestCustomizeViewActivity.java
@@ -0,0 +1,21 @@
+package com.ppx.dailystudy.test.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ppx.dailystudy.R;
+
+/**
+ * Author: LuoXia
+ * Date: 2021/3/29 15:23
+ * Description:
+ */
+public class TestCustomizeViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_customize_view);
+    }
+}
