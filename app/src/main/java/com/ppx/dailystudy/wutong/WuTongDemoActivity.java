eba0634e6b5d56efa947382bf339ae8631c8a3a1
LuoXia <v_luoxia@auto-pai.com>
2021-04-02 09:55:23 +0800

    feat: 梧桐车联代码测试demo
    梧桐车联代码测试demo

---
+++ app/src/main/java/com/ppx/dailystudy/wutong/WuTongDemoActivity.java
@@ -0,0 +1,29 @@
+package com.ppx.dailystudy.wutong;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ppx.dailystudy.R;
+
+/**
+ * @Author: LuoXia
+ * @Date: 2021/4/1 10:20
+ * @Description: 梧桐车联相关测试demo
+ */
+public class WuTongDemoActivity extends AppCompatActivity {
+
+    private String TAG = "WuTongDemoActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wutong_demo);
+
+        Log.d(TAG, "onCreate: " + getResources().getConfiguration().locale.getLanguage());
+
+
+    }
+}
