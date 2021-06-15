818937a2397f620d8c6609087fae6e63f299b2b9
xialuo <v_xialuo@tinnove.com.cn>
2021-06-15 17:54:19 +0800

    提交两个学习的git命令

--- app/src/main/java/com/ppx/dailystudy/test/GITTestClass.kt
+++ app/src/main/java/com/ppx/dailystudy/test/GITTestClass.kt
@@ -6,4 +6,10 @@ package com.ppx.dailystudy.test
  * @Description: 此类仅用于学习GIT命令时的测试
  */
 class GITTestClass {
+    /**
+     * 1、从暂存区撤销：git restore --staged file路径
+     * 2、撤销本地修改，代码将会被最近一次提交覆盖：git restore file
+     * 3、查看你已经配置的远程仓库服务器：git remote
+     * 4、添加远端仓库到本地：git remote -v originname url
+     */
 }
