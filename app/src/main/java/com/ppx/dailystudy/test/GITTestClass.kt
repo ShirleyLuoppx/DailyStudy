de70c8639f2668f944a3576dd2c8ae2be8edc6e2
xialuo <v_xialuo@tinnove.com.cn>
2021-06-16 10:10:50 +0800

    添加几个git命令

--- app/src/main/java/com/ppx/dailystudy/test/GITTestClass.kt
+++ app/src/main/java/com/ppx/dailystudy/test/GITTestClass.kt
@@ -9,7 +9,10 @@ class GITTestClass {
     /**
      * 1、从暂存区撤销：git restore --staged file路径
      * 2、撤销本地修改，代码将会被最近一次提交覆盖：git restore file
-     * 3、查看你已经配置的远程仓库服务器：git remote
-     * 4、添加远端仓库到本地：git remote -v originname url
+     * 3、查看你已经配置的远程仓库服务器简写：git remote
+     * 4、查看已经配置了的远程仓库服务器的简写和 仓库读写路径 ：  git remote -v
+     * 5、添加远端仓库到本地：git remote add originName url
+     * 6、拉取远端仓库的代码到本地：git fetch originName
+     * 7、推送提交记录到远端仓库：git push <remote> <branch>
      */
 }
