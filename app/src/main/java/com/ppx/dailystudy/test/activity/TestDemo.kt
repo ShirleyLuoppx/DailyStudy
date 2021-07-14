df7e1e76a7d18edd6ba693712600241b9ac75a8d
xialuo <v_xialuo@tinnove.com.cn>
2021-07-14 10:04:39 +0800

    测试handler的postdelay的run()里再调用handler.postDelay

--- app/src/main/java/com/ppx/dailystudy/test/activity/TestDemo.kt
+++ app/src/main/java/com/ppx/dailystudy/test/activity/TestDemo.kt
@@ -1,6 +1,9 @@
 package com.ppx.dailystudy.test.activity
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ppx.dailystudy.R
 import kotlinx.android.synthetic.main.activiyu_timepicker_test.*
@@ -10,7 +13,12 @@ import kotlinx.android.synthetic.main.activiyu_timepicker_test.*
  * Date: 2020/11/5 9:18
  * Description: 111
  */
-class TestDemo : AppCompatActivity(){
+class TestDemo : AppCompatActivity() {
+
+    private val TAG: String = "TestDemo"
+    private lateinit var mHandler: Handler
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activiyu_timepicker_test)
@@ -18,5 +26,25 @@ class TestDemo : AppCompatActivity(){
         timepicker.setOnTimeChangedListener { view, hourOfDay, minute ->
 
         }
+
+        /**
+         * 测试handler.postDelay的run()里面 再调用handler.postDelay
+         *
+         * 结论：延迟第一个postDelayed()的时间去调用run()里面的内容，然后run()里面的内容延迟循环调用
+         */
+        Log.d(TAG, "onCreate: 111111111111")
+        testDoublePostDelay()
+        mHandler = Handler(Looper.getMainLooper())
+        mHandler.postDelayed(object : Runnable {
+            override fun run() {
+                Log.d(TAG, "run: 3333333")
+                testDoublePostDelay()
+                mHandler.postDelayed(this, 5 * 1000)
+            }
+        }, 10 * 1000)
+    }
+
+    private fun testDoublePostDelay() {
+        Log.d(TAG, "testDoublePostDelay: 22222222222")
     }
 }
