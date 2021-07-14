e827d1b4a74086b2c725873e9e31826b233b7738
xialuo <v_xialuo@tinnove.com.cn>
2021-07-14 14:17:46 +0800

    测试git操作，场景:本地有已修改未暂存的文件，但是却需要在已稳定的版本上做出改动，是否可以先本地提交再切fix分支出去，再fix分支上回退到稳定版本后再修改讷

--- app/src/main/java/com/ppx/dailystudy/chatview/ChatAdapter.kt
+++ app/src/main/java/com/ppx/dailystudy/chatview/ChatAdapter.kt
@@ -8,6 +8,8 @@ import com.ppx.dailystudy.R
  * Author: luoxia
  * Date: 2020/8/26 23:26
  * Description: DESCRIPTION
+ * 
+ * 测试git11111
  */
 class ChatAdapter(data: MutableList<ChatViewBean>) :
     BaseQuickAdapter<ChatViewBean, BaseViewHolder>(R.layout.item_chat, data) {
--- app/src/main/java/com/ppx/dailystudy/chatview/ChatViewBean.kt
+++ app/src/main/java/com/ppx/dailystudy/chatview/ChatViewBean.kt
@@ -4,6 +4,8 @@ package com.ppx.dailystudy.chatview
  * Author: luoxia
  * Date: 2020/8/26 23:22
  * Description: 第二行代码-3.7-NinePatch的实例（本实例中并没有用到.9图相关的内容，只是单纯想实现一个这个聊天的样子而已）
+ *
+ * 测试git22222
  */
 class ChatViewBean {
     var msg: String = ""
