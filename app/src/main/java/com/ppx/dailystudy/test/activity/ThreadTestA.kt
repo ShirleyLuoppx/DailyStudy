f687abf329ec21bf24b48a3c5163c64bf1098c7a
xialuo <v_xialuo@tinnove.com.cn>
2021-08-05 16:38:44 +0800

    测试是否能在不同activity中操作同一个thread

--- app/src/main/java/com/ppx/dailystudy/test/activity/ThreadTestA.kt
+++ app/src/main/java/com/ppx/dailystudy/test/activity/ThreadTestA.kt
@@ -1,9 +1,34 @@
 package com.ppx.dailystudy.test.activity
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.ppx.dailystudy.R
+import kotlinx.android.synthetic.main.fragment_demo.*
+
 /**
  * @Author: LuoXia
  * @Date: 2021/8/5 16:07
  * @Description: 测试两个activity是否能对同一thread进行操作
+ * 结论：不能，在b活动引用的a活动的thread对象跟a活动中定义的thread对象不是同一个引用地址，且状态也不是同步的，b活动的状态会重新从NEW开始
  */
-class ThreadTestA {
+class ThreadTestA : AppCompatActivity() {
+
+    val thread: Thread = Thread()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_demo)
+
+        btn_thread_test.setOnClickListener {
+            startActivity(Intent(this, ThreadTestB::class.java))
+        }
+
+        Log.d("ThreadTestA", "onCreate: ${thread.state}---$thread")
+        Thread.sleep(2000)
+        thread.start()
+        Log.d("ThreadTestA", "onCreate: ${thread.state}---$thread")
+    }
 }
--- app/src/main/java/com/ppx/dailystudy/test/activity/ThreadTestB.kt
+++ app/src/main/java/com/ppx/dailystudy/test/activity/ThreadTestB.kt
@@ -1,9 +1,25 @@
 package com.ppx.dailystudy.test.activity
 
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.ppx.dailystudy.R
+import kotlin.concurrent.thread
+
 /**
  * @Author: LuoXia
  * @Date: 2021/8/5 16:08
  * @Description: 测试两个activity是否能对同一thread进行操作
  */
-class ThreadTestB {
+class ThreadTestB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val thread = ThreadTestA().thread
+        Log.d("ThreadTestB", "onCreate: ${thread.state}---$thread")
+
+        thread.start()
+        Log.d("ThreadTestB", "onCreate: ${thread.state}---$thread")
+    }
 }
