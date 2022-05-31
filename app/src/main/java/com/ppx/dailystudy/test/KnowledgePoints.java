79ed401d19f8ce6e9dad006b7c91b74c30aee240
xialuo <v_xialuo@tinnove.com.cn>
2022-05-31 16:03:58 +0800

    提交与、或、异或运算符的使用方式

--- app/src/main/java/com/ppx/dailystudy/test/KnowledgePoints.java
+++ app/src/main/java/com/ppx/dailystudy/test/KnowledgePoints.java
@@ -13,9 +13,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.ppx.dailystudy.R;
 
+import org.jetbrains.annotations.TestOnly;
+
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: LuoXia
@@ -31,7 +36,49 @@ public class KnowledgePoints extends AppCompatActivity {
 
 //        KotlinDemo.INSTANCE.filterTest();
 
-        testArraysFillFun();
+//        testArraysFillFun();
+//        singletonListTest();
+        AtomicIntegerSet();
+    }
+
+    /**
+     * 测试 AtomicInteger.set(1|3)
+     * 需要注意的是：这里的重点并不是AtomicInteger.set，而是set里面参数的  |  ,表示“或”，是需要将对应的十进制转换成二进制后，列竖式，有1 则为1 ，最终再转成十进制的结果
+     *
+     * eg：
+     *  AtomicInteger.set(1 | 22);
+     *  1         0001
+     *  22      1 0110
+     *  --------------
+     *  result  1 0111   -->对应的十进制则为23
+     *
+     *  总结：
+     *  | 或  ，有1则为1；
+     *  & 与  ，同为1则为1；
+     *  ^ 异或  ，不同为1则为1；
+     *  ~ 取反，这个有补码啥的，先记前三个吧
+     */
+    @TestOnly
+    public static void AtomicIntegerSet() {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        atomicInteger.set(1 | 22);
+        System.out.println("AtomicIntegerSet the last value is " + atomicInteger.get());
+    }
+
+    /**
+     * Collections.singletonList：只用一句代码将一个object类型的对象转换成为一个对应类型的List集合。
+     * params：object类型的参数
+     * return：对应类型的List集合
+     * <p>
+     * java 9及以后的版本中增加了一个：List.of();  支持传入1-10的参数，最后将所有参数都加入这个List集合。
+     * params：1-10个同类型的参数
+     * return：一个对应类型的List集合
+     */
+    public static void singletonListTest() {
+        List<Integer> integerList = Collections.singletonList(1);
+        for (Integer integer : integerList) {
+            System.out.println("singletonListTest:" + integer);
+        }
     }
 
     /**
