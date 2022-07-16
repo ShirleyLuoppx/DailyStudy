2098ff0bea66803b0f301d60c722a58dae511efb
xialuo <v_xialuo@tinnove.com.cn>
2022-07-16 17:05:12 +0800

    线程池ScheduledExecutorService的schedule的简单使用

--- app/src/main/java/com/ppx/dailystudy/thread/ExecutorServiceDemo.kt
+++ app/src/main/java/com/ppx/dailystudy/thread/ExecutorServiceDemo.kt
@@ -1,6 +1,8 @@
 package com.ppx.dailystudy.thread
 
+import android.util.Log
 import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
 /**
@@ -13,14 +15,14 @@ import java.util.concurrent.TimeUnit
 /**
  *
  * 为什么要引入线程池？
-    1.）new Thread()的缺点
-        每次new Thread()耗费性能
-        调用new Thread()创建的线程缺乏管理，被称为野线程，而且可以无限制创建，之间相互竞争，会导致过多占用系统资源导致系统瘫痪。
-        不利于扩展，比如如定时执行、定期执行、线程中断
-    2.）采用线程池的优点
-        重用存在的线程，减少对象创建、消亡的开销，性能佳
-        可有效控制最大并发线程数，提高系统资源的使用率，同时避免过多资源竞争，避免堵塞
-        提供定时执行、定期执行、单线程、并发数控制等功能
+1.）new Thread()的缺点
+每次new Thread()耗费性能
+调用new Thread()创建的线程缺乏管理，被称为野线程，而且可以无限制创建，之间相互竞争，会导致过多占用系统资源导致系统瘫痪。
+不利于扩展，比如如定时执行、定期执行、线程中断
+2.）采用线程池的优点
+重用存在的线程，减少对象创建、消亡的开销，性能佳
+可有效控制最大并发线程数，提高系统资源的使用率，同时避免过多资源竞争，避免堵塞
+提供定时执行、定期执行、单线程、并发数控制等功能
  */
 class ExecutorServiceDemo {
     private val TAG = "ExecutorServiceDemo"
@@ -211,15 +213,31 @@ class ExecutorServiceDemo {
         scheduledExecutorService.scheduleAtFixedRate(runnable, 2, 2, TimeUnit.SECONDS)
     }
 
+    fun test() {
+        println("test: start -----")
+        val runnable = Runnable {
+            run {
+                println("test: executorService.schedule -----")
+            }
+        }
 
+        val executorService: ScheduledExecutorService = Executors.newScheduledThreadPool(4)
+        executorService.schedule(runnable, 5, TimeUnit.SECONDS)
+
+        println("test: end -----")
+    }
 }
 
+
 fun main() {
+    println("main -----")
 //    ExecutorServiceDemo().newFixedThreadPoolTest()
 //    ExecutorServiceDemo().newCachedThreadPoolTest()
 //    ExecutorServiceDemo().newScheduledThreadPoolOneTimeTest()
 //    ExecutorServiceDemo().newScheduledThreadPoolDelayCircleTest()
 //    ExecutorServiceDemo().newSingleThreadExecutorTest()
 //    ExecutorServiceDemo().newSingleThreadScheduledExecutorTest()
-    ExecutorServiceDemo().newSingleThreadScheduledExecutorCircleTest()
+//    ExecutorServiceDemo().newSingleThreadScheduledExecutorCircleTest()
+
+    ExecutorServiceDemo().test()
 }
