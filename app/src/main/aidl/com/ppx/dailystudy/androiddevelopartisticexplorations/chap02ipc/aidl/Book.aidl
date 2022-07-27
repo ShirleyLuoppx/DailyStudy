3ba13003e045e107ffb672c97342d9e1b48eaf19
ShirleyLuo <1184793244@qq.com>
2022-07-27 22:21:47 +0800

    提交aidl 未完成

---
+++ app/src/main/aidl/com/ppx/dailystudy/androiddevelopartisticexplorations/chap02ipc/aidl/Book.aidl
@@ -0,0 +1,4 @@
+// Book.aidl
+package com.ppx.dailystudy;
+
+parcelable Book;
--- app/src/main/java/com/ppx/dailystudy/androiddevelopartisticexplorations/chap02ipc/UserSerializable.java
+++ app/src/main/java/com/ppx/dailystudy/androiddevelopartisticexplorations/chap02ipc/UserSerializable.java
@@ -6,6 +6,12 @@ import java.io.Serializable;
  * @Author: LuoXia
  * @CreateDate: 2022/7/18 22:46
  * @Description: 用于   用Serializable实现序列化和反序列化
+ *
+ * Serializable和Parcelable的区别和联系：
+ * 联系：都可用于序列化和反序列化
+ * 区别：Serializable是Java提供的序列化方式，使用简单，但开销较大，过程需要大量的IO操作；
+ *      Parcelable是Android提供的序列化方式，使用稍微难一点儿，但是效率很高，Android开发中，首选Parcelable。
+ *      但是当
  */
 public class UserSerializable implements Serializable {
 
