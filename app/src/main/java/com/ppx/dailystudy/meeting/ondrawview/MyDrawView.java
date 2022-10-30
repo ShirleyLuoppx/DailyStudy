b74c27575d1879d37b38f00673fe74a6aec78f07
ShirleyLuo <1184793244@qq.com>
2022-10-30 10:54:29 +0800

    test private git settings

---
+++ app/src/main/java/com/ppx/dailystudy/meeting/ondrawview/MyDrawView.java
@@ -0,0 +1,9 @@
+package com.ppx.dailystudy.meeting.ondrawview;
+
+/**
+ * @Author: LuoXia
+ * @Date: 2022/10/30 10:53
+ * @Description:
+ */
+public class MyDrawView {
+}
---
+++ app/src/main/java/com/ppx/dailystudy/meeting/remoteview/NotificationTest.java
@@ -0,0 +1,13 @@
+package com.ppx.dailystudy.meeting.remoteview;
+
+/**
+ * @Author: LuoXia
+ * @Date: 2022/10/30 10:45
+ * @Description: 消息通知
+ */
+public class NotificationTest {
+
+    public void defaultNotify() {
+
+    }
+}
