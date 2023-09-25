29adaf11e1d5e7f06c58ac3eecf992d9c4124d16
luoxia <1184793244@qq.com>
2023-09-25 15:08:01 +0800

    add kinds of usages of ShapesbleImageView

---
+++ app/src/main/java/com/ppx/dailystudy/material/ShapeImageviewDemoActivity.java
@@ -0,0 +1,23 @@
+package com.ppx.dailystudy.material;
+
+import com.ppx.dailystudy.R;
+import com.ppx.dailystudy.common.BaseActivity;
+
+/**
+ * @Author Shirley
+ * @Date：2023/9/25
+ * @Desc：  ShapeableImageView -- 各种形状的imageview，圆角的，圆形的，菱形的，扇形的，半圆形的，小船形的，tips形状的，酒杯形状的..可以尽情发挥想象力...
+ */
+public class ShapeImageviewDemoActivity extends BaseActivity {
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_shape_imageview;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
+
