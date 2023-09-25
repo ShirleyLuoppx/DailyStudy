0d126e890351b7661952551e7f6c447c4f45b39f
luoxia <1184793244@qq.com>
2023-09-25 10:54:12 +0800

    add drawerlayout+toolbat+navigation demo

--- app/src/main/java/com/ppx/dailystudy/material/drawerlayout/DrawerLayoutNavigationToolbarActivity.java
+++ app/src/main/java/com/ppx/dailystudy/material/drawerlayout/DrawerLayoutNavigationToolbarActivity.java
@@ -1,7 +1,16 @@
 package com.ppx.dailystudy.material.drawerlayout;
 
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 import com.ppx.dailystudy.R;
 import com.ppx.dailystudy.common.BaseActivity;
+
+import butterknife.BindView;
 
 /**
  * @Author Shirley
@@ -10,6 +19,13 @@ import com.ppx.dailystudy.common.BaseActivity;
  */
 public class DrawerLayoutNavigationToolbarActivity extends BaseActivity {
 
+    @BindView(R.id.navigation_toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.navigation)
+    NavigationView navigation;
+    @BindView(R.id.drawerlayout_navigation_toolbar)
+    DrawerLayout drawerLayoutNavigationToolbar;
+
     @Override
     protected int initLayout() {
         return R.layout.activity_drawerlayout_navigation_toolbar;
@@ -17,7 +33,17 @@ public class DrawerLayoutNavigationToolbarActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        toolbar.setNavigationOnClickListener(view -> finish());
 
+        navigation.setNavigationItemSelectedListener(item -> {
+            Toast.makeText(DrawerLayoutNavigationToolbarActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            return false;
+        });
+
+        //设置toolbar左侧  ["三" —— "←"]  的效果
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayoutNavigationToolbar,toolbar,R.string.open,R.string.close);
+        toggle.syncState();
+        drawerLayoutNavigationToolbar.addDrawerListener(toggle);
     }
 }
 
