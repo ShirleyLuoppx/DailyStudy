fc207724d4a0f0b06d98846d8069e06ecb5be9e2
luoxia <1184793244@qq.com>
2023-09-26 16:17:00 +0800

    add some assets xml icon

---
+++ app/src/main/java/com/ppx/dailystudy/test/activity/AssetsActivity.java
@@ -0,0 +1,23 @@
+package com.ppx.dailystudy.test.activity;
+
+import com.ppx.dailystudy.R;
+import com.ppx.dailystudy.common.BaseActivity;
+
+/**
+ * @Author Shirley
+ * @Date：2023/9/26
+ * @Desc：
+ */
+public class AssetsActivity extends BaseActivity {
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_assets;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
+
