b8f96b998e275ae9552653108154697714277d4e
luoxia <1184793244@qq.com>
2023-09-26 17:04:11 +0800

    add a toobar's simple demo

---
+++ app/src/main/java/com/ppx/dailystudy/material/ToolBarDemoActivity.java
@@ -0,0 +1,39 @@
+package com.ppx.dailystudy.material;
+
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.ppx.dailystudy.R;
+import com.ppx.dailystudy.common.BaseActivity;
+
+import butterknife.BindView;
+
+/**
+ * @Author Shirley
+ * @Date：2023/9/26
+ * @Desc：ToolBar 简单使用
+ */
+public class ToolBarDemoActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar_simple)
+    Toolbar toolbar_simple;
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_simple_toolbar;
+    }
+
+    @Override
+    protected void initView() {
+        toolbar_simple.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(ToolBarDemoActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+}
+
