93643ee7c00ab8842a696491756f00102b39f5b3
luoxia <1184793244@qq.com>
2023-10-27 11:14:25 +0800

    add usage of vbo

---
+++ app/src/main/java/com/ppx/dailystudy/opengl/vbo/VBOTestActivity.java
@@ -0,0 +1,23 @@
+package com.ppx.dailystudy.opengl.vbo;
+
+import com.ppx.dailystudy.R;
+import com.ppx.dailystudy.common.BaseActivity;
+
+/**
+ * @Author Shirley
+ * @Date：2023/10/27
+ * @Desc：
+ */
+public class VBOTestActivity extends BaseActivity {
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_vbo_test;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
+
---
+++ app/src/main/java/com/ppx/dailystudy/opengl/vbo/VboSurfaceView.java
@@ -0,0 +1,25 @@
+package com.ppx.dailystudy.opengl.vbo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.ppx.dailystudy.opengl.shape.CustomGLSurfaceView;
+
+/**
+ * @Author Shirley
+ * @Date：2023/10/27
+ * @Desc：
+ */
+public class VboSurfaceView extends CustomGLSurfaceView {
+
+    public VboSurfaceView(Context context) {
+        super(context);
+    }
+
+    public VboSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        VboRender render = new VboRender(context);
+        setRender(render);
+    }
+}
+
