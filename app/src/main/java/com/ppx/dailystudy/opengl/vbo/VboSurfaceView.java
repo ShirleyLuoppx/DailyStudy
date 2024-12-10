a39deb329f80128789c280e7a2a1d84f6befe85b
luoxia <1184793244@qq.com>
2024-12-10 10:45:14 +0800

    slove conflict 1

--- app/src/main/java/com/ppx/dailystudy/opengl/vbo/VboSurfaceView.java
+++ app/src/main/java/com/ppx/dailystudy/opengl/vbo/VboSurfaceView.java
@@ -7,7 +7,11 @@ import android.util.AttributeSet;
 =======
 import com.ppx.dailystudy.opengl.FboUseRender;
 import com.ppx.dailystudy.opengl.MyBitmapOrthogonalRender;
+<<<<<<< HEAD
 >>>>>>> 2539aee (add usage of bitmapOrthogonal)
+=======
+import com.ppx.dailystudy.opengl.WaterMarkRender;
+>>>>>>> e265f0a (add usage of adding a waterMark to a picture)
 import com.ppx.dailystudy.opengl.shape.CustomGLSurfaceView;
 
 /**
@@ -29,8 +33,13 @@ public class VboSurfaceView extends CustomGLSurfaceView {
 =======
         FboUseRender fboUseRender = new FboUseRender(context,900,1920);
         MyBitmapOrthogonalRender myBitmapOrthogonalRender = new MyBitmapOrthogonalRender(context);
+<<<<<<< HEAD
         setRender(myBitmapOrthogonalRender);
 >>>>>>> 2539aee (add usage of bitmapOrthogonal)
+=======
+        WaterMarkRender waterMarkRender = new WaterMarkRender(context);
+        setRender(waterMarkRender);
+>>>>>>> e265f0a (add usage of adding a waterMark to a picture)
     }
 }
 
