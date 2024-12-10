937755863e083f3402dc8c3e7f51118dcc4978d5
luoxia <1184793244@qq.com>
2024-12-10 10:39:12 +0800

    solve confilct

--- app/src/main/java/com/ppx/dailystudy/opengl/vbo/VboSurfaceView.java
+++ app/src/main/java/com/ppx/dailystudy/opengl/vbo/VboSurfaceView.java
@@ -3,6 +3,11 @@ package com.ppx.dailystudy.opengl.vbo;
 import android.content.Context;
 import android.util.AttributeSet;
 
+<<<<<<< HEAD
+=======
+import com.ppx.dailystudy.opengl.FboUseRender;
+import com.ppx.dailystudy.opengl.MyBitmapOrthogonalRender;
+>>>>>>> 2539aee (add usage of bitmapOrthogonal)
 import com.ppx.dailystudy.opengl.shape.CustomGLSurfaceView;
 
 /**
@@ -19,7 +24,13 @@ public class VboSurfaceView extends CustomGLSurfaceView {
     public VboSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
         VboRender render = new VboRender(context);
+<<<<<<< HEAD
         setRender(render);
+=======
+        FboUseRender fboUseRender = new FboUseRender(context,900,1920);
+        MyBitmapOrthogonalRender myBitmapOrthogonalRender = new MyBitmapOrthogonalRender(context);
+        setRender(myBitmapOrthogonalRender);
+>>>>>>> 2539aee (add usage of bitmapOrthogonal)
     }
 }
 
